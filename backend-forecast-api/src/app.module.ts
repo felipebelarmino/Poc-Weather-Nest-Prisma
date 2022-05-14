fdfeb7a4232aae5c9eb29540fb16cf7114ebdebd
felipebelarmino <epilefgomes1@gmail.com>
2022-05-14 11:23:37 -0300

    Created auth module - (backend)

--- backend-forecast-api/src/app.module.ts
+++ backend-forecast-api/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     AuthenticationModule,
     PrismaModule,
     UserModule,
+    AuthModule,
     
   ],
   controllers: [],
