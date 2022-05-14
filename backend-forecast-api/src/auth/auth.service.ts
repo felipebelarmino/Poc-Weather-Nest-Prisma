059cfefd8514631120ca6ce3fee8c5a4809017c7
felipebelarmino <epilefgomes1@gmail.com>
2022-05-14 12:02:19 -0300

    Added initial auth - (backend)

--- backend-forecast-api/src/auth/auth.service.ts
+++ backend-forecast-api/src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateUser(email: string, password: string) {
+      throw new Error('Method not implemented.');
+  }
+  async login(): Promise<any> {
+    return {
+      access_token: 'access-token-8f3ae836da744329a6f93b7e28cafced',
+    };
+  }
+}
