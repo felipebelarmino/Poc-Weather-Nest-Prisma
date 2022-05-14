55e66e65ab5be9d23dfe1177a0a40a1543cbedc1
felipebelarmino <epilefgomes1@gmail.com>
2022-05-14 10:42:53 -0300

    Create User, bcrypt - (backend)

--- backend-forecast-api/src/app.module.ts
+++ backend-forecast-api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     AuthenticationModule,
     PrismaModule,
+    UserModule,
     
   ],
   controllers: [],
---
+++ backend-forecast-api/src/user/dto/create-user.dto.ts
@@ -0,0 +1,28 @@
+import { User } from '../entities/user.entity';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto extends User {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+}
